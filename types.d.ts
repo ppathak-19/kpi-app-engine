684c56b7e668012afd59c6f32bb31dc3fecfde53
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-02-16 13:04:04 +0530

    created global type file

---
+++ types.d.ts
@@ -0,0 +1,25 @@
+import {
+  type EntityStub,
+  type EvidenceEvidenceType,
+} from "@dynatrace-sdk/client-classic-environment-v2";
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type AppCompProps = {};
+
+export type TableDataType = {
+  problemId: string;
+  displayName: string;
+  problemStartTime: string;
+  problemEndTime: string;
+  mttd: string;
+  mttr: string;
+};
+
+export type EvidenceArrayType = {
+  startTime: Date;
+  displayName: string;
+  entity: EntityStub;
+  evidenceType: EvidenceEvidenceType;
+  groupingEntity?: EntityStub | undefined;
+  rootCauseRelevant: boolean;
+};
