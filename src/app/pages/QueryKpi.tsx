b9b6a5a40b092e8f543ec776aaef6beac636f400
Priya Pathak <priya.pathak@I2LT482.systems.bgr.ionidea.com>
2024-02-21 17:01:03 +0530

    added table

--- src/app/pages/QueryKpi.tsx
+++ src/app/pages/QueryKpi.tsx
@@ -1,8 +1,16 @@
-import { Surface, TitleBar } from "@dynatrace/strato-components-preview";
+import {
+  DataTable,
+  Flex,
+  Heading,
+  Surface,
+  TableColumn,
+  TitleBar,
+} from "@dynatrace/strato-components-preview";
 import React, { useEffect, useState } from "react";
 import type { AppCompProps } from "types";
 import useGetKPIQueryData from "../hooks/useGetKPIQueryData";
 import useGetSummarizationData from "../hooks/useGetSummarizationData";
+import { queryKPITableColumn } from "../constants/problemTableColumns";
 
 const QueryKpi: React.FC<AppCompProps> = () => {
   const data = useGetKPIQueryData({ timeLine: "now()-10d" });
@@ -25,8 +33,26 @@ const QueryKpi: React.FC<AppCompProps> = () => {
   return (
     <Surface>
       <TitleBar>
-        <TitleBar.Title>Query KPI</TitleBar.Title>
+        <TitleBar.Title>KPI for Problems</TitleBar.Title>
       </TitleBar>
+      <Flex flexDirection="column" padding={20}>
+        <Heading level={2}>MTTD</Heading>
+        <DataTable
+          resizable
+          fullWidth
+          columns={queryKPITableColumn}
+          data={[]}
+        />
+      </Flex>
+      <Flex flexDirection="column" padding={20}>
+        <Heading level={2}>MTTR</Heading>
+        <DataTable
+          resizable
+          fullWidth
+          columns={queryKPITableColumn}
+          data={[]}
+        />
+      </Flex>
     </Surface>
   );
 };
