f5e6d9202e53b6f44679f34d20e588b6378dfd8a
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-02-26 19:16:09 +0530

    problem client with sdk

---
+++ src/app/components/ProblemsKPISDK.tsx
@@ -0,0 +1,73 @@
+import { problemsClient } from "@dynatrace-sdk/client-classic-environment-v2";
+import {
+  Container,
+  ProgressCircle,
+  ToggleButtonGroup,
+  ToggleButtonGroupItem,
+} from "@dynatrace/strato-components-preview";
+import React, { useEffect, useState } from "react";
+import type { AppCompProps, TableDataType } from "types";
+import { convertProbelmsData } from "../utils/convertProblemsData";
+import DataTableKpi from "./DataTableKpi";
+import LineTimeSeries from "./LineTimeSeries";
+
+const ProblemsKPISDK: React.FC<AppCompProps> = () => {
+  const [isDataLoaded, setDataLoaded] = useState(false);
+  const [tableDataForKPI, setTableDataForKPI] = useState<TableDataType[]>([]);
+  const [clickedBtn, setClickedBtn] = useState("line");
+
+  useEffect(() => {
+    const getListOfClosedProblems = async () => {
+      await problemsClient
+        .getProblems({
+          from: "now-1y/w",
+          problemSelector: "status(closed)",
+        })
+        .then((res) => {
+          const dataPromises = res.problems.map(
+            async (eachProblem) =>
+              await convertProbelmsData(eachProblem.problemId)
+          );
+
+          Promise.all(dataPromises)
+            .then((res) => {
+              setTableDataForKPI(res);
+              setDataLoaded(true);
+            })
+            .catch((err) => {
+              setTableDataForKPI([]);
+              setDataLoaded(true);
+              console.error("Error in Fetching Data", err.message);
+            });
+        });
+    };
+
+    getListOfClosedProblems();
+  }, []);
+  return (
+    <Container>
+      <ToggleButtonGroup value={clickedBtn} onChange={setClickedBtn}>
+        <ToggleButtonGroupItem value="dataTable">
+          Data Table
+        </ToggleButtonGroupItem>
+        <ToggleButtonGroupItem value="line">Line Series</ToggleButtonGroupItem>
+      </ToggleButtonGroup>
+
+      <br />
+      <br />
+      <br />
+      {isDataLoaded ? (
+        <>
+          {clickedBtn === "dataTable" && (
+            <DataTableKpi data={tableDataForKPI} />
+          )}
+          {clickedBtn === "line" && <LineTimeSeries data={tableDataForKPI} />}
+        </>
+      ) : (
+        <ProgressCircle />
+      )}
+    </Container>
+  );
+};
+
+export default ProblemsKPISDK;
