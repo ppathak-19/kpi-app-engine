6a787a31689c679632cfa6989553fe9d645b836b
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-02-27 12:20:28 +0530

    handling modals state in app comp

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,90 @@
-import { Page } from "@dynatrace/strato-components-preview";
-import React from "react";
+import { Flex, Page } from "@dynatrace/strato-components-preview";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import type { AppCompProps } from "types";
 import Header from "./components/Header";
-import { appRoutes } from "./constants/AppRoutes";
+import KPIButton from "./components/ReusableComponents/KPIButton";
+import KPIModal from "./components/ReusableComponents/KPIModal";
+import KPINumberInput from "./components/ReusableComponents/KPINumberInput";
+import { InformationModalData } from "./constants/ModalData";
+import Home from "./pages/Home";
 
 const App: React.FC<AppCompProps> = () => {
+  /** States For settings, info modal open and close  */
+  const [infoModalState, setInfoModalState] = useState(false);
+  const [settingsModalState, setSettingsModalState] = useState(false);
+  const [showNestedModal, setNestedModalState] = useState(false);
+
+  /** States For Baseline mttr,mttd values */
+  const [initialMttdValue, setMttdValue] = useState<number | null>(0);
+  const [initialMttrValue, setMttrValue] = useState<number | null>(0);
+
   return (
     <Page>
       <Page.Header>
-        <Header />
+        <Header
+          setInfoModalState={setInfoModalState}
+          setSettingModalState={setSettingsModalState}
+        />
       </Page.Header>
       <Page.Main>
+        {/* Any Additonal Routes Needed, you can add here */}
         <Routes>
-          {appRoutes.map((EachRoute) => (
-            <Route
-              key={EachRoute.path}
-              path={EachRoute.path}
-              element={EachRoute.element}
-            />
-          ))}
+          <Route element={<Home />} path="/" />
         </Routes>
+
+        {/* Modals For Info & Settings */}
+        <KPIModal
+          modalTitle="KPI Information"
+          open={infoModalState}
+          onClose={() => setInfoModalState(false)}
+        >
+          {InformationModalData}
+        </KPIModal>
+        <KPIModal
+          modalTitle="KPI Configuration Panel"
+          open={settingsModalState}
+          onClose={() => setSettingsModalState(false)}
+          footer={
+            <KPIButton
+              label="Save"
+              onClick={() => {
+                console.log({ initialMttdValue, initialMttrValue });
+                setNestedModalState(true);
+              }}
+            />
+          }
+        >
+          {/* Modal For User Input */}
+          <Flex gap={4}>
+            <Flex flexDirection="column" gap={12}>
+              <KPINumberInput
+                label="Baseline MTTD"
+                value={initialMttdValue}
+                onChange={setMttdValue}
+                placeholder="Enter baseline for MTTD"
+              />
+              <KPINumberInput
+                label="Baseline MTTR"
+                value={initialMttrValue}
+                onChange={setMttrValue}
+                placeholder="Enter baseline for MTTR"
+              />
+            </Flex>
+          </Flex>
+
+          {/* nested modal for successfull save or not */}
+          <KPIModal
+            modalTitle="Nested Modal"
+            open={showNestedModal}
+            onClose={() => {
+              setNestedModalState(false);
+              setSettingsModalState(false);
+            }}
+          >
+            {InformationModalData}
+          </KPIModal>
+        </KPIModal>
       </Page.Main>
     </Page>
   );
---
+++ src/app/constants/ModalData.ts
@@ -0,0 +1,14 @@
+export const InformationModalData = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+itaque. Nihil consectetur nesciunt, recusandae libero tempore ea nam,
+inventore dolore quasi minus doloribus saepe pariatur quas odit ab est
+velit! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+incidunt natus necessitatibus, eveniet ullam ad ducimus, tenetur
+pariatur perferendis dignissimos provident. Accusantium perspiciatis
+qui voluptatibus? A provident cum iure? Aut.
+
+
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+quisquam vel enim explicabo illo nisi illum reprehenderit quae,
+necessitatibus neque? Consequatur quas reprehenderit aperiam
+doloremque corrupti aut excepturi quam minus!`;
