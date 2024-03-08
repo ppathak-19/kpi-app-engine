1e0e69b420ae42d6ffd1bacb8126e2ad8fa7f9e7
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-03-08 13:57:27 +0530

    updated app routes

--- src/app/constants/AppRoutes.tsx
+++ src/app/constants/AppRoutes.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import type { appRoutesType } from "types";
 import Data from "../pages/Data";
 import Home from "../pages/Home";
-import ProblemEvents from "../pages/ProblemEvents";
 import QueryKpi from "../pages/QueryKpi";
-import ProblemsKPISDK from "../components/ProblemsKPISDK";
 
 export const appRoutes: appRoutesType[] = [
   {
@@ -18,19 +16,9 @@ export const appRoutes: appRoutesType[] = [
     label: "Data Visualizations",
   },
   {
-    path: "/problem-events-v1",
-    element: <ProblemEvents />,
-    label: "DQL Problem Events",
-  },
-  {
     path: "/kpi-query-v2",
     // path: "/",
     element: <QueryKpi />,
     label: "KPI Query V2",
-  },
-  {
-    path: "/problem-events-sdk",
-    element: <ProblemsKPISDK />,
-    label: "Problems SDK",
   },
 ];
