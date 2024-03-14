f6b01c925faca9205b4845fc6b63f106455528bc
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-03-14 13:08:53 +0530

    added baseline thresholds and formatter for y-axis

--- src/app/pages/QueryKpi.tsx
+++ src/app/pages/QueryKpi.tsx
@@ -4,18 +4,25 @@ import {
   Heading,
   TimeseriesChart,
 } from "@dynatrace/strato-components-preview";
+import Colors from "@dynatrace/strato-design-tokens/colors";
 import React, { useState } from "react";
 import type { AppCompProps, aggregationsType } from "types";
 import MetricDetailSection from "../components/MetricDetailSection";
 import useGetKPIMetrices from "../hooks/useGetKPIMetrices";
 import { giveTimeseriesData } from "../utils/giveTimeseriesData";
 import { getBeforePastDays, getPastDaysRange } from "../utils/timeConverters";
+import { useMetricsContext } from "../hooks/context/MetricsContext";
 
 const QueryKpi: React.FC<AppCompProps> = () => {
+  /** States For Two Dropdowns for the app */
   const [selectTimeFrame, setSelectTimeFrame] = useState<string>("2");
   const [clickedAggreation, setClickedAggregation] =
     useState<aggregationsType>("min");
 
+  /** taking values from context, to give thresholds to timeseries comp */
+  const { initialMttdValue, initialMttrValue } = useMetricsContext();
+
+  /** getting timeline1 */
   const getTimeLine1 = getPastDaysRange(selectTimeFrame);
 
   /** Getting Metrices for Last 2 Days */
@@ -26,10 +33,12 @@ const QueryKpi: React.FC<AppCompProps> = () => {
     shouldUseTimeFrame2: true,
   });
 
+  /** taking value from `MetricsDetailSection` and setting selected timeframe value to state */
   const handleTimeFrameChange = (time: string) => {
     setSelectTimeFrame(time);
   };
 
+  /** taking value from `MetricsDetailSection` and setting selected aggregation value to state */
   const handleAggregationChange = (clickedVal: aggregationsType) => {
     setClickedAggregation(clickedVal);
   };
@@ -46,6 +55,7 @@ const QueryKpi: React.FC<AppCompProps> = () => {
   return (
     <>
       <Flex>
+        {/* First Container of app */}
         <MetricDetailSection
           daysData={daysData}
           selectedTimeFrame={selectTimeFrame}
@@ -57,6 +67,7 @@ const QueryKpi: React.FC<AppCompProps> = () => {
       <br />
       <br />
       <br />
+      {/* Second part of app */}
       <Heading level={6}>Current Days- Timeseries</Heading>
       <br />
       <Flex flexDirection="row" width="100%" justifyContent="space-around">
@@ -65,36 +76,34 @@ const QueryKpi: React.FC<AppCompProps> = () => {
             data={CurrentTimeseriesMttd}
             variant="area"
             loading={daysData.isLoading}
-          />
+          >
+            <TimeseriesChart.Threshold
+              data={{ value: initialMttdValue }}
+              label="Baseline MTTD"
+              color={Colors.Charts.Threshold.Bad.Default}
+            />
+            <TimeseriesChart.YAxis
+              formatter={(value) => `${Math.round(value)} mins`}
+            />
+          </TimeseriesChart>
         </Container>
         <Container variant="minimal" width="42%">
           <TimeseriesChart
             data={CurrentTimeseriesMttr}
             variant="area"
             loading={daysData.isLoading}
-          />
+          >
+            <TimeseriesChart.Threshold
+              data={{ value: initialMttrValue }}
+              label="Baseline MTTR"
+              color={Colors.Charts.Threshold.Bad.Default}
+            />
+            <TimeseriesChart.YAxis
+              formatter={(value) => `${Math.round(value)} mins`}
+            />
+          </TimeseriesChart>
         </Container>
       </Flex>
-      <br />
-      <br />
-      {/* <Heading level={6}>Relative Days- Timeseries</Heading>
-      <br /> */}
-      {/* <Flex flexDirection="row" width="100%" justifyContent="space-around">
-        <Container variant="minimal" width="40%">
-          <TimeseriesChart
-            data={RelativeTimeseriesMttd}
-            variant="area"
-            loading={daysData.isLoading}
-          />
-        </Container>
-        <Container variant="minimal" width="40%">
-          <TimeseriesChart
-            data={RelativeTimeseriesMttr}
-            variant="area"
-            loading={daysData.isLoading}
-          />
-        </Container>
-      </Flex> */}
     </>
   );
 };
