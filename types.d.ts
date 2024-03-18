c0a9f2af62655cad4e74fedcff847878e59684d3
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-03-18 19:05:50 +0530

    unistalled classicV2 package and removed types

--- types.d.ts
+++ types.d.ts
@@ -1,39 +1,7 @@
-import {
-  type EntityStub,
-  type EvidenceEvidenceType,
-} from "@dynatrace-sdk/client-classic-environment-v2";
 import type { QueryResult } from "@dynatrace-sdk/client-query";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type AppCompProps = {};
-
-export type TableDataType = {
-  problemId: string;
-  displayName: string;
-  problemStartTime: string;
-  problemEndTime: string;
-  mttd: string;
-  mttr: string;
-};
-
-export type EvidenceArrayType = {
-  startTime: Date;
-  displayName: string;
-  entity: EntityStub;
-  evidenceType: EvidenceEvidenceType;
-  groupingEntity?: EntityStub | undefined;
-  rootCauseRelevant: boolean;
-};
-
-export type ProblemEvidenceLineChartType = {
-  name: string;
-  unit: string;
-  datapoints: {
-    start: Date;
-    end: Date;
-    value: string;
-  }[];
-};
 
 /** Application Routing Type */
 export type appRoutesType = {
