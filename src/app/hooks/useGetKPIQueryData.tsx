f4b026b4d6b9fa0627c01edaae95b156bab7cb15
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-03-20 11:25:46 +0530

    created a builder fun to get main query

--- src/app/hooks/useGetKPIQueryData.tsx
+++ src/app/hooks/useGetKPIQueryData.tsx
@@ -1,8 +1,9 @@
 import { type QueryResult } from "@dynatrace-sdk/client-query";
 import { useDqlQuery } from "@dynatrace-sdk/react-hooks";
 import type { QueryProps } from "types";
+import { buildAppMainQuery } from "../utils/appQueries";
 
-/** This Hook Returns the `QueryResult of the provided timeline / timeframe` */
+/** This Hook Returns the `QueryResult of the provided timeframe` */
 const useGetKPIQueryData = (
   props: QueryProps
 ): {
@@ -14,45 +15,13 @@ const useGetKPIQueryData = (
 
   const queryData1 = useDqlQuery({
     body: {
-      query: `// fetching events in given timeline/timeframe
-      fetch events, ${`timeframe:"${timeLine1}"`}
-      | filter event.kind == "DAVIS_PROBLEM" and event.status == "CLOSED" and event.status_transition == "CLOSED"
-      | sort  timestamp desc
-      | expand dt.davis.event_ids // expanding the array of davis events
-      | fieldsKeep event.start, event.end,resolved_problem_duration,dt.davis.event_ids,event.id, display_id, timestamp
-      | fieldsAdd res = lookup([
-        fetch events, ${`timeframe:"${timeLine1}"`}
-          | filter event.kind == "DAVIS_EVENT"
-          |  sort timestamp asc
-          | fields event.id, event.kind, event.start]
-      , sourceField: dt.davis.event_ids, lookupField: event.id)
-      | fieldsFlatten res // flattening the response of lookup
-      | sort  res.event.start asc // sorting the response with ascending order
-      | dedup  event.id // removing the duplicates of event id's
-      | filter res != "null" // removing records having res == null
-      `,
+      query: buildAppMainQuery(timeLine1),
     },
   });
 
   const queryData2 = useDqlQuery({
     body: {
-      query: `// fetching events in given timeline/timeframe
-      fetch events, ${`timeframe:"${timeLine2}"`}
-      | filter event.kind == "DAVIS_PROBLEM" and event.status == "CLOSED" and event.status_transition == "CLOSED"
-      | sort  timestamp desc
-      | expand dt.davis.event_ids // expanding the array of davis events
-      | fieldsKeep event.start, event.end,resolved_problem_duration,dt.davis.event_ids,event.id, display_id, timestamp
-      | fieldsAdd res = lookup([
-        fetch events, ${`timeframe:"${timeLine2}"`}
-          | filter event.kind == "DAVIS_EVENT"
-          |  sort timestamp asc
-          | fields event.id, event.kind, event.start]
-      , sourceField: dt.davis.event_ids, lookupField: event.id)
-      | fieldsFlatten res // flattening the response of lookup
-      | sort  res.event.start asc // sorting the response with ascending order
-      | dedup  event.id // removing the duplicates of event id's
-      | filter res != "null" // removing records having res == null
-      `,
+      query: buildAppMainQuery(timeLine2),
     },
   });
 
