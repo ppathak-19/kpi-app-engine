1c562d73fd5862db6adf644889ce5d2d782ad78a
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-03-21 12:24:05 +0530

    handled errors in app and implemented a fallback `ui`

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -4,7 +4,12 @@ import {
   Surface,
 } from "@dynatrace/strato-components-preview/layouts-core";
 import { Tooltip } from "@dynatrace/strato-components-preview/overlays";
-import { Text } from "@dynatrace/strato-components-preview/typography";
+import {
+  Heading,
+  Paragraph,
+  Text,
+} from "@dynatrace/strato-components-preview/typography";
+import { ExpandableText } from "@dynatrace/strato-components-preview/content";
 import { WarningIcon } from "@dynatrace/strato-icons";
 import React from "react";
 import { useAppContext } from "../hooks/Context-API/AppContext";
@@ -17,43 +22,59 @@ type HomeCompProps = {
 const Home: React.FC<HomeCompProps> = (props) => {
   const { setModalState } = props;
 
-  const { state } = useAppContext();
+  const { state, error } = useAppContext();
   const isUserInputtedData =
     !!state.baseline.mttd &&
     state.baseline.mttd !== 0 &&
     !!state.baseline.mttr &&
     state.baseline.mttr !== 0;
 
+  const isErrorInApp = error.isError;
+
   return (
     <Surface>
-      {isUserInputtedData ? (
+      {isErrorInApp ? (
         <>
-          <QueryKpi />
+          <Heading level={4}>Oops, An Error Occured</Heading>
+          <br />
+          <ExpandableText>
+            <Paragraph>
+              {error.errorDetails.code}-{error.errorDetails.message}
+            </Paragraph>
+          </ExpandableText>
         </>
       ) : (
-        <Surface height={400}>
-          <Flex
-            style={{
-              textAlign: "center",
-              cursor: "pointer",
-            }}
-            justifyContent="center"
-            alignContent="center"
-          >
-            <Flex>
-              <Button color="warning" size="condensed">
-                <Tooltip text="Click Here to add values">
-                  <WarningIcon />
-                </Tooltip>
-              </Button>
-            </Flex>
-            <Flex onClick={() => setModalState(true)}>
-              <Text>
-                Click Here to add the BaseLine Values For Calculations
-              </Text>
-            </Flex>
-          </Flex>
-        </Surface>
+        <>
+          {isUserInputtedData ? (
+            <>
+              <QueryKpi />
+            </>
+          ) : (
+            <Surface height={400}>
+              <Flex
+                style={{
+                  textAlign: "center",
+                  cursor: "pointer",
+                }}
+                justifyContent="center"
+                alignContent="center"
+              >
+                <Flex>
+                  <Button color="warning" size="condensed">
+                    <Tooltip text="Click Here to add values">
+                      <WarningIcon />
+                    </Tooltip>
+                  </Button>
+                </Flex>
+                <Flex onClick={() => setModalState(true)}>
+                  <Text>
+                    Click Here to add the BaseLine Values For Calculations
+                  </Text>
+                </Flex>
+              </Flex>
+            </Surface>
+          )}
+        </>
       )}
     </Surface>
   );
