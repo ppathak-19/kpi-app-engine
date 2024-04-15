0a184f95192484fdd0021f4d1e4e060053d85e9d
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-04-15 17:55:14 +0530

    updated app main query supporting 1yr tf

--- src/app/utils/appQueries.ts
+++ src/app/utils/appQueries.ts
@@ -1,18 +1,79 @@
-export const buildAppMainQuery = (timeFrame: string) => `\
-// fetching events in given timeline/timeframe
-      fetch events, timeframe:"${timeFrame}"
-      | filter event.kind == "DAVIS_PROBLEM" and event.status == "CLOSED" and event.status_transition == "CLOSED"
-      | sort  timestamp desc
-      | expand dt.davis.event_ids // expanding the array of davis events
-      | fieldsKeep event.start, event.end,resolved_problem_duration,dt.davis.event_ids,event.id, display_id, timestamp, event.category
-      | fieldsAdd res = lookup([
-        fetch events, timeframe:"${timeFrame}"
-          | filter event.kind == "DAVIS_EVENT"
-          |  sort timestamp asc
-          | fields event.id, event.kind, event.start]
-      , sourceField: dt.davis.event_ids, lookupField: event.id)
-      | fieldsFlatten res // flattening the response of lookup
-      | sort  res.event.start asc // sorting the response with ascending order
-      | dedup  event.id // removing the duplicates of event id's
-      | filter res != "null" // removing records having res == null
+// export const buildAppMainQuery = (timeFrame: string) => `\
+// // fetching events in given timeline/timeframe
+//       fetch events, timeframe:"${timeFrame}"
+//       | filter event.kind == "DAVIS_PROBLEM" and event.status == "CLOSED" and event.status_transition == "CLOSED"
+//       | sort  timestamp desc
+//       | expand dt.davis.event_ids // expanding the array of davis events
+//       | fieldsKeep event.start, event.end,resolved_problem_duration,dt.davis.event_ids,event.id, display_id, timestamp, event.category
+//       | fieldsAdd res = lookup([
+//         fetch events, timeframe:"${timeFrame}"
+//           | filter event.kind == "DAVIS_EVENT"
+//           |  sort timestamp asc
+//           | fields event.id, event.kind, event.start]
+//       , sourceField: dt.davis.event_ids, lookupField: event.id)
+//       | fieldsFlatten res // flattening the response of lookup
+//       | sort  res.event.start asc // sorting the response with ascending order
+//       | dedup  event.id // removing the duplicates of event id's
+//       | filter res != "null" // removing records having res == null
+// `;
+
+/** Radu Query */
+// export const buildAppMainQuery = (timeFrame: string) => `\
+// // fetching events in given timeline/timeframe
+// fetch dt.davis.problems.snapshots, timeframe: "${timeFrame}"
+//   | filter (event.status_transition == "CLOSED" OR event.status_transition == "CREATED")
+//   | fieldsKeep event.start, event.end,resolved_problem_duration,dt.davis.event_ids,event.id, display_id, timestamp,event.category
+//   | expand  dt.davis.event_ids
+//   | fieldsRename mykey = dt.davis.event_ids
+//   | append [
+//     fetch  dt.davis.events.snapshots, timeframe: "${timeFrame}"
+//     | fieldsRename mykey = event.id
+//     | fieldsAdd firsteventstart = event.start
+//     | sort firsteventstart asc
+//   ]
+//   | summarize {
+//     mttr = max(resolved_problem_duration),
+//     event.start = takeFirst(event.start),
+//     event.end = takeFirst(event.end),
+//     event.category = takeAny(event.category),
+//     display_id = takeAny(display_id),
+//     res.event.start = takeLast(firsteventstart),
+//     timestamp = takeFirst(timestamp)
+//   }, by: { mykey }
+//   | fieldsAdd mttd = event.start - res.event.start
+//   | filter isNotNull(mttr) and isNotNull(display_id) and isNotNull(mttd)
+// `;
+
+export const buildAppMainQuery = (timeline: string) => `\
+fetch dt.davis.problems.snapshots, timeframe: "${timeline}"
+  | filter (event.status_transition == "CLOSED" OR event.status_transition == "CREATED")
+  | fieldsKeep event.start, event.end,resolved_problem_duration,dt.davis.event_ids,event.id, display_id, timestamp,event.category
+  | expand  dt.davis.event_ids
+  | fieldsRename mykey = dt.davis.event_ids
+  | filter isNotNull(event.end)
+  | append [
+    fetch  dt.davis.events.snapshots
+    | fieldsAdd mykey = event.id , firsteventstart = event.start
+    | sort firsteventstart asc
+  ]
+  | summarize {
+    mttr = max(resolved_problem_duration),
+    event.start = takeFirst(event.start),
+    event.end = takeFirst(event.end),
+    event.category = takeAny(event.category),
+    display_id = takeAny(display_id),
+    res.event.start = takeMin(firsteventstart),
+    res.event.kind = takeLast(event.kind),
+    res.event.id = takeLast(event.id),
+    timestamp = takeFirst(timestamp),
+    event.id = takeFirst(event.id)
+  }, by: { mykey }
+  | fieldsAdd mttd = event.start - res.event.start
+  | filter isNotNull(mttr) and isNotNull(display_id) and isNotNull(mttd)
+  | sort mttd desc
+  | dedup display_id
+  | limit 9999
 `;
+
+// eslint-disable-next-line no-secrets/no-secrets
+// timestamp4/8/2024, 3:30:59 PMdt.davis.event_ids-2789155000165963988_1712566500000resComplex recordevent.id-2789155000165963988_1712566500000V2res.event.id-2789155000165963988_1712566500000res.event.kindDAVIS_EVENTres.event.start4/8/2024, 2:25 PMres.event.status_transitionCREATEDmttd5 mindisplay_idP-2404269event.categoryERRORevent.end4/8/2024, 3:00 PMevent.start4/8/2024, 2:30 PMresolved_problem_duration30 min
